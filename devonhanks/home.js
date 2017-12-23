41eca634d4b34479c59b69bedc3af9aaebd17be6
Santhosh Gugan <santhoshg1990@gmail.com>
2017-12-23 18:18:47 +0530

    commit 1

---
+++ devonhanks/home.js
@@ -0,0 +1,7 @@
+$(document).ready(function($) {
+	/*
+	//on hover of the headers 
+	$('.dh_navigation_links span').hover(function() {
+		$(this).parent().children('span').not($(this)).css("color":"#888");
+	});*/
+});
